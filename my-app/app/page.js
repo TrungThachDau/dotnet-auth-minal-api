50d82df29537e5787ae718467d36e779e1ccc5f7
Nguyen Quang Trung <qtrung1702@outlook.com>
2025-08-07 15:51:36 +0000

    update

---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {Button} from "antd"
+
+export default function Home() {
+  return (
+    <>
+    
+      <Button type="primary">Ok</Button>
+    </>
+  );
+}
